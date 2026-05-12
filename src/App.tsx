a71a1a0dfef68d3fa1f1263d3c6891f30b917cb5
Nemes Tamás - CAD1 <nemestamas94@gmail.com>
2026-05-12 10:24:41 +0200

    Fetch and display activity days from JSON data

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,34 @@
+import { useEffect, useState } from "react"
+
+type Activity = {
+  name: string,
+  timeInSec: number
+}
+
+type ActivityDay = {
+  date: string,
+  activities: Activity[]
+}
+
 const App = () => {
+  const [activityDays, setActivityDays] = useState<ActivityDay[]>()
+
+  useEffect(()=>{
+    fetch("data.json")
+    .then(response => response.json())
+    .then(data => setActivityDays(data))
+  },[])
+
   return (
-    <div>App</div>
+    <>
+    {
+      activityDays ?
+      <div>
+        {activityDays.map(day => <div>{day.date}</div>)}
+      </div> :
+      <div>Loading...</div>
+    }
+    </>
   )
 }
 
